b2d137ca214a5f713d5b176a1dafa16d0c6408af
Marcin Slusarek <mrvrota@gmail.com>
2023-09-24 19:51:53 +0200

    fix date

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="metadata_manager",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     install_requires=[
        'arrow'
